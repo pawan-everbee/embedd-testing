9bc05c06f1f1fa616a90436afa2d60b24b5f94d6
pawan-everbee <128399324+pawan-everbee@users.noreply.github.com>
2025-08-07 19:27:45 +0530

    Create my_script.js

---
+++ my_script.js
@@ -0,0 +1,10 @@
+function myfunction() {
+	const button = document.getElementsByClassName("pdp-add-to-cart")[0];
+	if (button) {
+		button.textContent = "modified by script";
+	} else {
+		console.warn("Button not found");
+	}
+}
+
+myfunction()
