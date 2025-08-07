b1ec4e6f997ae3e60ab364ef3079a5397f815bec
pawan-everbee <128399324+pawan-everbee@users.noreply.github.com>
2025-08-07 19:38:26 +0530

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,9 +1,10 @@
+console.log('Script logic running ✅');
 
-const button = document.getElementsByClassName("pdp-add-to-cart")[0];
-if (button) {
-	button.textContent = "modified by script";
-} else {
-	console.warn("Button not found");
-}
-
-console.log('see');
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementsByClassName("pdp-add-to-cart")[0];
+  if (button) {
+    button.textContent = "modified by script";
+  } else {
+    console.warn("Button not found ❌");
+  }
+});
