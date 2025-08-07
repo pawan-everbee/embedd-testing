53134b2b7afba6645d9229c3fa1f73c3fb5bc77b
pawan-everbee <128399324+pawan-everbee@users.noreply.github.com>
2025-08-07 20:09:48 +0530

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,6 +1,7 @@
 console.log('Script logic running ✅');
 
 document.addEventListener("DOMContentLoaded", function () {
+	console.log('buttons', document.getElementsByClassName());
 	const button = document.getElementsByClassName("pdp-add-to-cart")[0];
 	if (button) {
 		button.textContent = "modified by script";
