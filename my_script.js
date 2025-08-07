025da736d4f19e38e63d987e2a179954b7457f0f
pawan-everbee <128399324+pawan-everbee@users.noreply.github.com>
2025-08-07 19:36:17 +0530

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -1,10 +1,9 @@
-function myfunction() {
-	const button = document.getElementsByClassName("pdp-add-to-cart")[0];
-	if (button) {
-		button.textContent = "modified by script";
-	} else {
-		console.warn("Button not found");
-	}
+
+const button = document.getElementsByClassName("pdp-add-to-cart")[0];
+if (button) {
+	button.textContent = "modified by script";
+} else {
+	console.warn("Button not found");
 }
+
 console.log('see');
-myfunction()
